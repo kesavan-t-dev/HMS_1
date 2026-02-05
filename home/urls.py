473abcb470a15bba140cd6949b31f3ec611acc55
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-02-05 15:18:35 +0530

    add vercel files

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.root_redirect),  
-    path('home/', views.book_appointment, name='home'),
+    path('', views.book_appointment, name='home'),
     path('success/<uuid:booking_id>/', views.booking_success, name='success'),
 ]
